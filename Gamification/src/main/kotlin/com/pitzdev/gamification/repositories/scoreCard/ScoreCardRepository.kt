778c69641bb28b39b20e592a0864e5d36a12190c
Eduardo Pitz <pitz.eduardo@gmail.com>
2020-03-24 12:44:40 -0300

    Criando ScoreCardRepository

--- Gamification/src/main/kotlin/com/pitzdev/gamification/repositories/scoreCard/ScoreCardRepository.kt
+++ Gamification/src/main/kotlin/com/pitzdev/gamification/repositories/scoreCard/ScoreCardRepository.kt
@@ -1,12 +1,22 @@
 package com.pitzdev.gamification.repositories.scoreCard
 
-import com.pitzdev.gamification.models.badgeCard.BadgeCard
+import com.pitzdev.gamification.models.leaderBord.LeaderBoard
+import com.pitzdev.gamification.models.scoreCard.ScoreCard
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ScoreCardRepository : CrudRepository<BadgeCard, Long> {
+interface ScoreCardRepository : CrudRepository<ScoreCard, Long> {
 
-    fun findByUserIdOrderByDateCreatedDesc(userId: Long): List<BadgeCard>
+    @Query("SELECT SUM(s.score) FROM ScoreCard s WHERE s.userId = :userId GROUP BY s.userId")
+    fun getTotalScoreForUser(@Param("userId") userId: Long) : Int
+
+
+    @Query("SELECT LeaderBoard(s.userId, SUM(s.score)) FROM ScoreCard s GROUP BY s.userId ORDER BY SUM(s.score) DESC")
+    fun findFirst10(): List<LeaderBoard>
+
+    fun findByUserIdOrderByByDateCreatedDesc(userId: Long): List<ScoreCard>
 
 }
