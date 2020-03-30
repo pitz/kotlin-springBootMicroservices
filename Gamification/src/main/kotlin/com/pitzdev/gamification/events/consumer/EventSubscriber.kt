f035e85d425911cb8790e8d98a2dfb2f5e60608f
Pitz <pitz.eduardo@gmail.com>
2020-03-30 20:48:08 -0300

    Run EventSubscriber ao subir o serviço.

--- Gamification/src/main/kotlin/com/pitzdev/gamification/events/consumer/EventSubscriber.kt
+++ Gamification/src/main/kotlin/com/pitzdev/gamification/events/consumer/EventSubscriber.kt
@@ -1,15 +1,36 @@
 package com.pitzdev.gamification.events.consumer
 
+import com.pitzdev.gamification.services.game.GameServiceImpl
 import com.rabbitmq.client.AMQP
 import com.rabbitmq.client.ConnectionFactory
 import com.rabbitmq.client.DefaultConsumer
 import com.rabbitmq.client.Envelope
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
 
-class EventSubscriber() {
+@Component
+class EventSubscriber(private val gameServiceImpl: GameServiceImpl) : CommandLineRunner {
 
     private val EVENT_HOST = "localhost"
 
-    private fun startLstener(queueName: String, callback: (String) -> Unit) {
+    override fun run(vararg args: String?) {
+        println("EventSubscriber.multiplicationSolvedListener() :: Starting listener.")
+        multiplicationSolvedListener()
+        println("EventSubscriber.multiplicationSolvedListener() :: Listener started.")
+    }
+
+    private fun multiplicationSolvedListener() {
+        consume("multiplication.solved", callback())
+    }
+
+    private fun callback() : (String) -> Unit {
+        return { body: String ->
+            println(" JSON: $body")
+            gameServiceImpl.newAttemptForUser(1, 1, false) // fakeData
+        }
+    }
+
+    private fun consume(queueName: String, callback: (String) -> Unit) {
         val factory = ConnectionFactory()
         factory.host = EVENT_HOST
 
@@ -17,6 +38,7 @@ class EventSubscriber() {
         val channel = connection.createChannel()
 
         channel.queueDeclare(queueName, false,false,false,null)
+
         val consumer = object : DefaultConsumer(channel) {
             override fun handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: ByteArray) {
                 callback(String(body, charset("UTF-8")))
@@ -26,10 +48,4 @@ class EventSubscriber() {
         channel.basicConsume(queueName, true, consumer)
     }
 
-    fun callListener() {
-        val queueName: String = "multiplication.solved"
-        val callback: (String) -> Unit = { body: String -> println(" JSON recebido: $body") }
-
-        startLstener(queueName, callback)
-    }
 }
